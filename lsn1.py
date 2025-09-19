d473777a7bbcfb6c7fb41e23d017b3fe4dd68498
abjdev <91409956+abjdev@users.noreply.github.com>
2025-09-19 22:19:33 +0300

    Create the Lesson1 Script

---
+++ lsn1.py
@@ -0,0 +1,23 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            
+            }
+
+while True:
+    word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    elif word == "liste":
+        print(meme_dict)
+    else:
+        print("ben de bilmiyorum.")
+        addinp = input("Bu kelimeyi sözlüğe eklemek ister misin?")
+        if addinp == "evet" or addinp == "Evet" or addinp == "EVET":
+            desc = input("Anlamı ne?")
+            meme_dict[word] = desc
+        else:
+            continue
+print("Baybay!")
+    
